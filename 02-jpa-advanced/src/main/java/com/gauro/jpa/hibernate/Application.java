334607227a3e392c3f7f27844081dae7be8cd5d9
Chandra kishor Gauro <ckgauro@gmail.com>
2021-03-16 11:24:05 -0400

    Advanced JPA

---
+++ 02-jpa-advanced/src/main/java/com/gauro/jpa/hibernate/Application.java
@@ -0,0 +1,13 @@
+package com.gauro.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
